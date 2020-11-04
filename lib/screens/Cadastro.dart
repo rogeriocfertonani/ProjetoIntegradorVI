76e237a4293a33bf58bb095dfefdc4bffe6c84c2
manoel <manogiraldes@gmail.com>
2020-11-04 14:46:10 -0300

    cadastro de donatario

--- lib/screens/Cadastro.dart
+++ lib/screens/Cadastro.dart
@@ -6,24 +6,95 @@ class Cadastro extends StatefulWidget {
   _CadastroState createState() => _CadastroState();
 }
 
+@override
+
 class _CadastroState extends State<Cadastro> {
+  final _formKeyScreen = GlobalKey<FormState>();
+  FocusNode myFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode = FocusNode();
+    print("passou no focusnode");
+  }
+  @override
+  void dispose() {
+    // Clean up the focus node when the Form is disposed.
+    myFocusNode.dispose();
+
+    super.dispose();
+  }
   void cadastrar(){
 
-    Firestore.instance
+//    Firestore.instance
+//        .collection("Instituicao")
+//        .document("endereco")
+//        .setData({"rua":"Angelina","numero":40});
+
+//    print("cadastrou!!!!!!!");
+    Map<String,dynamic> mapainclusao;
+    mapainclusao = {
+      "Nome": this.nome,
+      "Endereco":this.endereco,
+      "email":this.email,
+      "CPF":this.CPF
+    };
+
+//    Firestore.instance
+//        .collection("Instituicao")
+//        .document("dadosPessoais")
+//
+//        .setData(mapainclusao);
+    DocumentReference documentReference = Firestore.instance
         .collection("Instituicao")
-        .document("endereco")
-        .setData({"rua":"Angelina","numero":40});
-
+        .document();
+    documentReference.setData(mapainclusao).whenComplete(() => {
+      _limparFormulario(),
+      _showDialog()});
     print("cadastrou!!!!!!!");
-
   }
 
-  String nome;
-  _getNome(String nome){
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // retorna um objeto do tipo Dialog
+        return AlertDialog(
+          title: new Text("Cadastro"),
+          content: new Text("Cadastramento de donatário efetuado com sucesso !"),
+          actions: <Widget>[
+            // define os botões na base do dialogo
+            new FlatButton(
+              child: new Text("Fechar"),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  String nome,endereco,email,CPF;
+
+  _getNome(nome){
     this.nome = nome;
 
   }
+  _getEndereco(endereco){
+    this.endereco = endereco;
+  }
 
+  _getEmail(email){
+    this.email = email;
+
+  }
+  _getCPF(CPF){
+    this.CPF = CPF;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,35 +104,49 @@ class _CadastroState extends State<Cadastro> {
           appBar: AppBar(title:Text('Cadastro')),
           body:SingleChildScrollView(
             padding: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
-            child: Form(child:Column(
+            child: Form(
+              key:_formKeyScreen,
+              child:Column(
               children: [
 
                 TextFormField(
-                  keyboardType:TextInputType.text,
-                  decoration: InputDecoration(
-                    labelText: 'Nome',
+                    keyboardType:TextInputType.text,
+                    focusNode: myFocusNode,
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
 
-                  ),
-                  onChanged: _getNome(nome) ,
+                    ),
+                    onChanged:(String nome){ _getNome(nome);}
                 ),
 
                 TextFormField(
-                  keyboardType:TextInputType.text,
-                  decoration: InputDecoration(
-                    labelText: 'Endereço',
-                  ),),
+                    keyboardType:TextInputType.text,
+                    decoration: InputDecoration(
+                      labelText: 'Endereço',
+                    ),
+                    onChanged:(String endereco){ _getEndereco(endereco);}
+                ),
                 TextFormField(
-                  keyboardType:TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'email',
-                  ),),
+                    keyboardType:TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'email',
+                    ),
+                    onChanged:(String email){ _getEmail(email);}
+
+                ),
                 TextFormField(
-                  keyboardType:TextInputType.number,
-                  decoration: InputDecoration(
-                    labelText: 'CPF',
-                  ),),
+                    keyboardType:TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'CPF',
+                    ),
+                    onChanged:(String CPF){ _getCPF(CPF);}
+                ),
                 RaisedButton(onPressed: cadastrar,
-                  child: Text('Cadastrar'),)
+                  child: Text('Cadastrar'),
+
+
+
+                )
 
 
               ],
@@ -78,20 +163,26 @@ class _CadastroState extends State<Cadastro> {
       ],
     );
   }
+
+  _limparFormulario() {
+
+    _formKeyScreen.currentState?.reset();
+    myFocusNode.requestFocus();
+  }
 }
 
 class Cadastro_old extends StatelessWidget {
 
- void cadastrar(){
+  void cadastrar(){
 
-  Firestore.instance
-      .collection("Instituicao")
-      .document("endereco")
-      .setData({"rua":"Angelina","numero":40});
+    Firestore.instance
+        .collection("Instituicao")
+        .document("endereco")
+        .setData({"rua":"Angelina","numero":40});
 
-  print("cadastrou!!!!!!!");
+    print("cadastrou!!!!!!!");
 
- }
+  }
 
 
   @override
